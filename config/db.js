efbdf73602bce9ee1c8dcd634436119dcc06e161
Arpit <guptaarpit3334@gmail.com>
2023-08-16 12:21:44 +0530

    initial

---
+++ config/db.js
@@ -0,0 +1,18 @@
+
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+//Connecting MongoDB
+function connectDB(){
+
+    mongoose.connect(process.env.MONGO_CONNECTION_URL, {useNewUrlParser: true, useCreateIndex: true , useUnifiedTopology:true , useFindAndModify : true});
+
+    const connection = mongoose.connection;
+
+    connection.once('open', ()=> {
+        console.log('Database connected.')
+    })
+    
+}
+
+module.exports =connectDB;
